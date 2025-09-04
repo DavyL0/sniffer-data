b6099d7f4f4e7ca9f16b10bc9efac5836b951557
Davy Lopes <d4vyl0pes@gmail.com>
2025-09-04 20:03:16 -0300

    feat:create java project

---
+++ sniffer-data/src/test/java/com/davy/snifferdata/SnifferDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davy.snifferdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnifferDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
