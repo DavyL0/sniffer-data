60b3fe222732a87d43d2442cdcb10c5126e7aac3
Davy Lopes <d4vyl0pes@gmail.com>
2025-09-05 14:14:18 -0300

    feat: add list get map

--- sniffer-data/src/main/java/com/davy/snifferdata/controller/LogController.java
+++ sniffer-data/src/main/java/com/davy/snifferdata/controller/LogController.java
@@ -1,4 +1,27 @@
 package com.davy.snifferdata.controller;
 
+import com.davy.snifferdata.models.Log;
+import com.davy.snifferdata.services.LogService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class LogController {
+    private final LogService logService;
+
+    public LogController(LogService logService) {
+        this.logService = logService;
+    }
+
+    @PostMapping
+    public Log save(@RequestBody Log log){
+        return logService.saveLog(log);
+    }
+
+    @GetMapping
+    public List<Log> findAll(){
+        return logService.findAll();
+    }
 }
