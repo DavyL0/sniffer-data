3df2d5dd3d8d33f01421a79b823b293e5553e2da
Davy Lopes <d4vyl0pes@gmail.com>
2025-09-05 14:12:17 -0300

    feat: add repository annotation

--- sniffer-data/src/main/java/com/davy/snifferdata/repositories/LogRepository.java
+++ sniffer-data/src/main/java/com/davy/snifferdata/repositories/LogRepository.java
@@ -2,6 +2,8 @@ package com.davy.snifferdata.repositories;
 
 import com.davy.snifferdata.models.Log;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LogRepository extends MongoRepository<Log,String> {
 }
